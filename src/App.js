0ec3c76251f44015f772e508a0092f87b20b5b1a
Cberumen51 <c.berumen51@gmail.com>
2020-01-26 22:03:13 -0800

    Got API working, got links working, added card decoration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./components/Header.js";
+import { Route } from "react-router-dom";
+import WelcomePage from './components/WelcomePage'
+import CharacterList from "./components/CharacterList.js";
 
 
 export default function App() {
+  const [chars, setchars] = useState([])
+   
+
   return (
     <main>
       <Header />
+      <Route path="/" exact>
+        <WelcomePage chars={chars} setchars={setchars} />
+        </Route> 
+        <Route path="/character/:id">
+         <CharacterList chars={chars} setchars={setchars} />
+         </Route>
     </main>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 export default function SearchForm() {
+  const []
  
   return (
     <section className="search-form">
